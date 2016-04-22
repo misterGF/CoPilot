d12eda089dc0b25279bfa7311dfe65659d626982
Gil Ferreira <gfgilly@gmail.com>
2016-04-22 15:21:29 -0400

    Coming soon message for servers

--- src/components/dash/Server.vue
+++ src/components/dash/Server.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="text-center">Server</h1>
-  <h4 class="text-center">List of our servers.</h4>
+  <h4 class="text-center">Coming soon!</h4>
   <section class="content">
     <div class="row">
       <div class="col-md-12">
