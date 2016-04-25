dda5ff5c89f463c135cdf7882fd7d52258028cf9
Gil Ferreira <gfgilly@gmail.com>
2016-04-25 15:51:38 -0400

    Added link to github repo

--- src/components/dash/Dashboard.vue
+++ src/components/dash/Dashboard.vue
@@ -3,6 +3,11 @@
   <section class='content'>
     <!-- Info boxes -->
     <div class='row'>
+      <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-check"></i> CoPilot is open source!</h4>
+        Click on icon to check out it out on github. <a href='https://github.com/misterGF/CoPilot' target='_blank'><i class='fa fa-github fa-2x'></i></a>
+      </div>
       <div class='col-md-3 col-sm-6 col-xs-12'>
         <div class='info-box'>
           <span class='info-box-icon bg-aqua'><i class='ion ion-ios-gear-outline'></i></span>
