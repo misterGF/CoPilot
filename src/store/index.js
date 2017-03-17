2d833e92c438426d50c0fd477e9e5e2e780b3ac5
euvl <yev.vlasenko@gmail.com>
2017-03-17 11:12:39 +0000

    Moved store mutations and actions into separate files

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import state from './state'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  actions,
+  mutations
+})
---
+++ src/store/state.js
@@ -0,0 +1,12 @@
+export default const state = {
+  callingAPI: false,
+  searching: '',
+  serverURI: 'http://10.110.1.136:8080',
+  user: null,
+  token: null,
+  userInfo: {
+    messages: [{1: 'test', 2: 'test'}],
+    notifications: [],
+    tasks: []
+  }
+}
