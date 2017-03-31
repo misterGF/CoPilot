f37e648633b9a55594271e8cb2a5fba89b5ad6d2
John D. Lewis <balinjdl+github@gmail.com>
2017-03-31 14:11:06 -0400

    Fixed link to repo
    Moved href tag around button tag; original sequence didn't work on
    Firefox 52.0.2 (Windows)

--- src/components/views/Repos.vue
+++ src/components/views/Repos.vue
@@ -27,9 +27,9 @@
                   </div>
                   <div class="col-sm-4 border-right">
                     <div class="description-block">
-                      <button type="button" class="btn btn-block btn-default btn-lg">
-                        <a v-bind:href="repo.owner.html_url" target="_blank">Visit</a>
-                      </button>
+                      <a v-bind:href="repo.owner.html_url" target="_blank">
+                        <button type="button" class="btn btn-block btn-default btn-lg">Visit</button>
+                      </a>
                     </div>
                   </div>
                   <div class="col-sm-4">
