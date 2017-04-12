033a5a09c48f548acc4e5d27c35906bc758204df
Gil Ferreira <Gfgilly@gmail.com>
2017-04-12 14:46:38 -0400

    Added a _redirects file for netlify to properly load subpages if a user refreshes. Build also updated to copy file.

--- build/build.js
+++ build/build.js
@@ -23,6 +23,7 @@ var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirect
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
 cp('-R', 'static/*', assetsPath)
+cp('_redirects', config.build.assetsRoot)
 
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
