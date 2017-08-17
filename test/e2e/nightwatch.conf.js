a8c0916c3e4a2a5c89323fba072d197d1a7b161b
Sebastian Hultstrand <sebastian.hultstrand@smspark.se>
2017-08-17 18:37:50 +0200

    Added geckodriver to fix e2e in firefox

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -10,7 +10,8 @@ module.exports = {
     'host': '127.0.0.1',
     'port': 4444,
     'cli_args': {
-      'webdriver.chrome.driver': require('chromedriver').path
+      'webdriver.chrome.driver': require('chromedriver').path,
+      'webdriver.geckodriver.driver': require('chromedriver').path
     }
   },
 
