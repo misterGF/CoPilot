985a4ebd8fdbfe79dc94447087e3ded2f966a216
Gil Ferreira <Gfgilly@gmail.com>
2019-01-03 12:30:09 -0500

    Removed scss from notification item vue as well

--- src/components/layout/NotificationItem.vue
+++ src/components/layout/NotificationItem.vue
@@ -24,33 +24,25 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.navbar-custom-menu > .navbar-nav > li.notifications-menu > .dropdown-menu > li > ul.menu {
-  li > a > h4,
-  li > a > p {
-    margin: 0;
-    white-space: normal;
-    word-break: break-word;
-  }
-
-  li > a > h4 {
-    font-size: 15px;
-
-    small {
-      font-size: 10px;
-    }
-  }
-
-  li > a > p {
-    font-size: 12px;
-  }
-
-  li > a > h4 > span {
-    display: inline-block;
-  }
-
-  li > a > h4 > small.time {
-    margin-top: 3px;
-  }
+<style scoped>
+.navbar-custom-menu > .navbar-nav > li.notifications-menu > .dropdown-menu > li > ul.menu li > a > h4, .navbar-custom-menu > .navbar-nav > li.notifications-menu > .dropdown-menu > li > ul.menu li > a > p {
+  margin: 0;
+  white-space: normal;
+  word-break: break-word;
+}
+.navbar-custom-menu > .navbar-nav > li.notifications-menu > .dropdown-menu > li > ul.menu li > a > h4 {
+  font-size: 15px;
+}
+.navbar-custom-menu > .navbar-nav > li.notifications-menu > .dropdown-menu > li > ul.menu li > a > h4 small {
+  font-size: 10px;
+}
+.navbar-custom-menu > .navbar-nav > li.notifications-menu > .dropdown-menu > li > ul.menu li > a > p {
+  font-size: 12px;
+}
+.navbar-custom-menu > .navbar-nav > li.notifications-menu > .dropdown-menu > li > ul.menu li > a > h4 > span {
+  display: inline-block;
+}
+.navbar-custom-menu > .navbar-nav > li.notifications-menu > .dropdown-menu > li > ul.menu li > a > h4 > small.time {
+  margin-top: 3px;
 }
 </style>
